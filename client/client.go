fc27eccc44b390ed01295902657b9bb7aab35b43
Mark Ferree <markferree@gmail.com>
2016-10-17 20:59:21 -0700

    Saving some wit

--- client/client.go
+++ client/client.go
@@ -6,7 +6,7 @@ import "fmt"
 func main() {
 	resp, err := http.Get("http://127.0.0.1:9999/witticism")
 	if err != nil {
-		// handle error
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 	fmt.Println(resp)
--- server/server.go
+++ server/server.go
@@ -2,11 +2,18 @@ package main
 
 import "net/http"
 import "fmt"
-import "html"
+import "math/rand"
 import "log"
 
 func witHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	var witticism [5]string
+	witticism[0] = "He has all the virtues I dislike and none of the vices I admire."
+	witticism[1] = "There is no such thing as public opinion. There is only published opinion."
+	witticism[2] = "Without courage all virtues lose their meaning."
+	witticism[3] = "A fanatic is one who can't change his mind and won't change the subject."
+	witticism[4] = "The first quality that is needed is audacity."
+
+	fmt.Fprintf(w, "%q", witticism[rand.Intn(5)])
 }
 
 func main() {
