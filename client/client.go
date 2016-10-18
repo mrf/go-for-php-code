0ac41ba9f31e7136a5ffe8b3bf75dc8b9cef7d34
Mark Ferree <markferree@gmail.com>
2016-10-18 12:22:03 -0700

    Concurrent example of client code

--- client/client.go
+++ client/client.go
@@ -5,14 +5,24 @@ import "fmt"
 import "io/ioutil"
 
 func main() {
-	resp, err := http.Get("http://127.0.0.1:9999/witticism")
-	if err != nil {
-		fmt.Println(err)
+	count := 1
+	messages := make(chan string, 2)
+	for count < 5000 {
+		go func(msg string) {
+			resp, err := http.Get("http://127.0.0.1:9999/witticism")
+			if err != nil {
+				fmt.Println(err)
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+			resp.Body.Close()
+			messages <- string(body)
+		}("getting")
+		fmt.Println(count)
+		msg := <-messages
+		fmt.Println(msg)
+		count++
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(body))
 }
